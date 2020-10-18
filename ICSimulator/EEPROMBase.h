4ad85bf4e588a0880f9f86e49725660510f7994e
YapuWU <47836048+YapuWU@users.noreply.github.com>
2020-10-18 15:22:18 +1100

    Atmel Studio project created

---
+++ ICSimulator/EEPROMBase.h
@@ -0,0 +1,21 @@
+/*
+ * EEPROMBase.h
+ *
+ * Created: 18/10/2020 3:21:33 PM
+ *  Author: Yarpo
+ */ 
+
+
+#ifndef EEPROMBASE_H_
+#define EEPROMBASE_H_
+
+class EEPROMBase
+{
+	public:
+	protected:
+	private:
+};
+
+
+
+#endif /* EEPROMBASE_H_ */
---
+++ ICSimulator/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ICSimulator.cpp
+ *
+ * Created: 18/10/2020 3:16:54 PM
+ * Author : Yarpo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
